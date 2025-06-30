998ac236fa8f50487c483fb8b70948cd670c5353
taro-git <t.taro30954694@gmail.com>
2025-06-30 19:47:51 +0900

    delete code unrelated to the logic

--- server/app_data_collecter/services/common/GetBoxScoreDataService.py
+++ server/app_data_collecter/services/common/GetBoxScoreDataService.py
@@ -1,4 +1,3 @@
-import datetime
 from ...models.nba_api.BoxScoreDataModel import BoxScoreData
 from ...models.postgres.BoxScoreDataForPostgresModel import BoxScoreDataForPostgres
 from ..nba_api.BoxScoreDataNbaApiService import BoxScoreDataNbaApiService
@@ -10,18 +9,13 @@ class GetBoxScoreDataService:
         self.boxScoreDataNbaApiService = boxScoreDataNbaApiService or BoxScoreDataNbaApiService(game_id=game_id)
 
     def get_box_score_data(self) -> BoxScoreData:
-        start_time = datetime.datetime.now()
         box_score_data_from_postgres = BoxScoreDataForPostgres.objects.filter(game_id=self.game_id).first()
         if box_score_data_from_postgres == None:
             box_score_data_from_nba_api = self.boxScoreDataNbaApiService.get_box_score_data()
             box_score_data_from_nba_api_for_postgres: BoxScoreDataForPostgres = box_score_data_from_nba_api.to_postgres_model()
             box_score_data_from_nba_api_for_postgres.upsert()
-            time_diff = (datetime.datetime.now() - start_time).total_seconds()
-            print(f"return box score data from nba_api. time: {time_diff}")
             return box_score_data_from_nba_api
         else:
-            time_diff = (datetime.datetime.now() - start_time).total_seconds()
-            print(f"return box score data from postgres. time: {time_diff}")
             return BoxScoreData.from_postgres_model(box_score_data_from_postgres)
 
 
