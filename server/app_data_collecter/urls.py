8b760cff924baddbbf08d0bce1ed30d0ea33d5b0
taro-git <t.taro30954694@gmail.com>
2025-07-12 18:24:00 +0900

    edit django url path

--- server/app_data_collecter/urls.py
+++ server/app_data_collecter/urls.py
@@ -7,9 +7,9 @@ from .views.NBABoxScoreSummaryView import NBABoxScoreSummaryView
 from .views.NBABoxScoreDataView import NBABoxScoreDataView
 
 urlpatterns = [
-    path('api/v1/clickhouse-test/', clickhouse_test),
-    path('api/v1/nba/game-summaries/', NBAGameSummariesView.as_view()),
-    path('api/v1/nba/box-score-summary/', NBABoxScoreSummaryView.as_view()),
-    path('api/v1/nba/box-score-data/', NBABoxScoreDataView.as_view()),
-    path('api/v1/nbaapi-test/', NBAApiTest.as_view()),
+    path('clickhouse-test/', clickhouse_test),
+    path('nba/game-summaries/', NBAGameSummariesView.as_view()),
+    path('nba/box-score-summary/', NBABoxScoreSummaryView.as_view()),
+    path('nba/box-score-data/', NBABoxScoreDataView.as_view()),
+    path('nbaapi-test/', NBAApiTest.as_view()),
 ]
--- server/project_box_score_arc/urls.py
+++ server/project_box_score_arc/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_data_collecter.urls')),
+    path('api/v1/', include('app_data_collecter.urls')),
 ]
